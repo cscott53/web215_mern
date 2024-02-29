82eaff6fa49e1b8ac041a5a9cf375405fa859e9d
cscott53 <127893200+cscott53@users.noreply.github.com>
2024-02-29 10:31:53 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function App() {
+  return /*#__PURE__*/React.createElement("h1", null, "MERN - MongoDB, Express, React, Node");
+}
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
