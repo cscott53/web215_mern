d4b035559e8ce91f80330245714ef03bf86922a9
cscott53 <127893200+cscott53@users.noreply.github.com>
2024-04-14 15:59:26 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require("express"),
       path = require('path'),
       router = require('./api')
       app = express(),
-      port = 3000
+      port = process.env.PORT || 3000
 app.use('/api',router)
 app.use(express.static(path.join(__dirname,'../client/build')))
 app.get(['/','/contest/:contestid'],(req,res)=>res.sendFile(path.join(__dirname,'../client/build','index.html')))
-app.listen(port,()=>console.log(`Server running at port ${port}. PID: ${process.pid}`))
+app.listen(port,()=>console.log(`Server running at port ${port}`))
