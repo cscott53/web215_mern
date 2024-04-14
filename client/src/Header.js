fcfa68a3f6a858cafd9d8bce09105505619d4e65
cscott53 <127893200+cscott53@users.noreply.github.com>
2024-04-14 17:27:30 -0400

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,16 @@
 export default function Header({message}) {
     //
     return (
-        <h1>Charles Scott - Curious Squirrel - WEB215</h1>
-        <h2 className='header'>{message}</h2>
-        <span class="links header"><a href="https://cscott53.github.io/web215/">Home</a></span>
-        <span class="links header"><a href="https://cscott53.github.io/web215/introduction.html">Introduction</a></span>
-        <span class="links header"><a href="https://cscott53.github.io/web215/contract.html">Contract</a></span>
-        <span class="links header"><a href="https://cscott53.github.io/web215/brand.html">Brand</a></span>
-        <span class="links header"><a href="https://cscott53.github.io/web215/introduction_form.html">Intro form</a></span>
-        <span class="links header"><a href="https://web215-m10-cookies.onrender.com">Intro form 2</a></span>
-        <span class="links header"><a href="https://web215-mern.onrender.com">MERN stack</a></span>
+        <header>
+            <h1>Charles Scott - Curious Squirrel - WEB215</h1>
+            <h2 className='header'>{message}</h2>
+            <span class="links header"><a href="https://cscott53.github.io/web215/">Home</a></span>
+            <span class="links header"><a href="https://cscott53.github.io/web215/introduction.html">Introduction</a></span>
+            <span class="links header"><a href="https://cscott53.github.io/web215/contract.html">Contract</a></span>
+            <span class="links header"><a href="https://cscott53.github.io/web215/brand.html">Brand</a></span>
+            <span class="links header"><a href="https://cscott53.github.io/web215/introduction_form.html">Intro form</a></span>
+            <span class="links header"><a href="https://web215-m10-cookies.onrender.com">Intro form 2</a></span>
+            <span class="links header"><a href="https://web215-mern.onrender.com">MERN stack</a></span>
+        </header>
     )
 }
