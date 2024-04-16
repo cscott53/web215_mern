dc6c7314365052393760005fe76abb3d52f4a3f5
cscott53 <127893200+cscott53@users.noreply.github.com>
2024-04-15 21:48:26 -0400

    Update showData.js

--- showData.js
+++ showData.js
@@ -1,4 +1,4 @@
-let {connectClient, stopClient} = require('./db')
+let {connectClient, stopClient} = require('./db'); //I usually don't use semicolons but this time I had to since the IIFE (immediately invoked function expression) below surrounded by () seeems to make JS think I'm trying to invoke what's returnede from require() thus throwing an error
 (async()=>{
     let db = await connectClient()
     let collections = db.listCollections().toArray()
