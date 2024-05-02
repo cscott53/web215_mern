67c91f24149b80a967a787f0acaa19864c2d5f9b
cscott53 <cscott53@email.cpcc.edu>
2024-05-02 19:48:03 -0400

    Updated header

--- client/src/Header.js
+++ client/src/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
             <span class="links header"><a href="https://cscott53.github.io/web215/introduction_form.html">Intro form</a></span>
             <span class="links header"><a href="https://web215-mern.onrender.com">MERN stack</a></span>
             <span class="links header"><a href="https://web215-m11-login.onrender.com">Login/sign up</a></span>
+            <span class="links header"><a href="https://web215-m12-project.onrender.com">Project (journaling app)</a></span>
         </header>
     )
 }
